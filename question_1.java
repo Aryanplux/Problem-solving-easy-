c76a30e84da7d9fc159c11b2e53b9a5390c37867
Aryan Dhiman <146617531+Aryanplux@users.noreply.github.com>
2025-02-22 23:01:19 +0530

    Update question_1.java

---
+++ question_1.java
@@ -0,0 +1,17 @@
+// Enter 3 numbers from the user & make a function to print their average.
+
+	import java.util.*;
+
+
+public class Solutions {
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+
+
+      int average = (a + b + c) / 3;
+      System.out.println(average);
+   }   
+}
