c7c18c92d5acc04239972a4c30e28fe62128f307
Aryan Dhiman <146617531+Aryanplux@users.noreply.github.com>
2025-02-22 23:04:10 +0530

    Update question_6.java

---
+++ question_6.java
@@ -0,0 +1,12 @@
+// Write an infinite loop using do while condition.
+import java.util.*;
+
+
+public class Solutions {
+   public static void main(String args[]) {
+      do {
+
+
+      } while(true);
+   }   
+}
